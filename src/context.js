20a5ecf58ca866626dc44efe3c56468ecbe37de4
Jack <meidan.roskaposti@gmail.com>
2025-10-04 23:14:03 +0300

    Update context.js: Support for #ask and #refresh

--- src/context.js
+++ src/context.js
@@ -1,32 +1,34 @@
-// Example of context-file
-// Note: Adding JackPreprocess() is enough!
+// Note: Adding function calls to JackPreprocess() and JackAskAiQuestion() is enough!
+//      (JackRemoveSystemMsg() is needed for debuging.)
 
 // === Remove debug messages ===
 function JackRemoveSystemMsg(text) {
     if (state.debugMode) {
-        text = text.replace(/<SYSTEM>[\s\S]*?<\/SYSTEM>/g, '').trim();
         state.lastOutput = state.lastOutput.replace(/<SYSTEM>[\s\S]*?<\/SYSTEM>/g, '').trim();
         state.debugOutput = "\nDebugOut: \n";
     } else {
         state.debugOutput = "";
     }
-    return text;
+    return text.replace(/<SYSTEM>[\s\S]*?<\/SYSTEM>/g, '');
 }
   
 // === CONTEXT-hook (data sent to AI) ===
 const modifier = (text) => {
   
-    // Remove debug messages (optional)
+    // Remove debug messages
     text = JackRemoveSystemMsg(text);
 
-    // Just add following line to support JackPreprocess
-    // (+ copy everything from library.js to your library-file)
+    // C-style Preprosessing of the context
     text = JackPreprocess(text);
 
-    // Store Context (not used by Jack)
+    // Needed to support #ASK and #REFRESH directives
+    // which will send questions to AI
+    text = JackAskAiQuestion(text);
+
+    // Store Context
     state.lastContext = text;
     
     //return {text, stop};
     return {text};
-};
+}
 modifier(text);
