233d1419ebdd4957cc5948f0f82b3a83dadacf28
Mr-Jack-Black <meidan.roskaposti@gmail.com>
2025-10-04 10:57:09 +0300

    Update context.js (add: remove debug msg)

--- src/context.js
+++ src/context.js
@@ -1,13 +1,32 @@
-// Example of simplifistic context-file
-const modifier = (text) => {
+// Example of context-file
+// Note: Adding JackPreprocess() is enough!
 
-  // Just add following line to support JackPreprocess
-  // (+ copy everything from library.js to your library-file)
-  text = JackPreprocess(text);
+// === Remove debug messages ===
+function JackRemoveSystemMsg(text) {
+    if (state.debugMode) {
+        text = text.replace(/<SYSTEM>[\s\S]*?<\/SYSTEM>/g, '').trim();
+        state.lastOutput = state.lastOutput.replace(/<SYSTEM>[\s\S]*?<\/SYSTEM>/g, '').trim();
+        state.debugOutput = "\nDebugOut: \n";
+    } else {
+        state.debugOutput = "";
+    }
+    return text;
+}
   
-  // Store Context
-  state.lastContext = text;
+// === CONTEXT-hook (data sent to AI) ===
+const modifier = (text) => {
+  
+    // Remove debug messages (optional)
+    text = JackRemoveSystemMsg(text);
+
+    // Just add following line to support JackPreprocess
+    // (+ copy everything from library.js to your library-file)
+    text = JackPreprocess(text);
+
+    // Store Context (not used by Jack)
+    state.lastContext = text;
     
-  return {text};
+    //return {text, stop};
+    return {text};
 };
 modifier(text);
