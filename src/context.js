915375da79afc4d385ef1152bf6e339e5ca953cb
Jack <meidan.roskaposti@gmail.com>
2025-10-28 00:01:19 +0200

    Update context.js to demonstrate AC integration

--- src/context.js
+++ src/context.js
@@ -1,16 +1,29 @@
 // Note: Adding function call to JackPreprocess() is enough
+// but JackAskAiQuestion is needed for AI questions (#ask/#asking)
 
 // === CONTEXT-hook (data sent to AI) ===
 const modifier = (text) => {
+    stop = false; // Ai Processing needed always by default.
 
     // C-style Preprosessing of the context
-    text = JackPreprocess(text);
+    text = JackPreprocessor(text);
 
     // Needed to support #ASK and #REFRESH directives
-    // which will send questions to AI
+    // which will send questions to AI.
     text = JackAskAiQuestion(text);
+/*
+    // Do not execute Auto-Cards if there is AI question.
+    if (!JackThereIsActiveAiQuestion()) {
+        // LewdLeah Auto-Cards
+        [text, stop] = AutoCards("context", text, stop);
+    }
+*/
+    // Optional: Log Context that was output
+    if (state.verboseLevel >= LOG_CONTEXT) {
+        state.debugOutput += "\n\nCONTEXT_OUT:\n============\n" + text + "\n============\n";
+    }
     
-    //return {text, stop};
-    return {text};
+    //return {text};
+    return {text, stop};
 }
 modifier(text);
